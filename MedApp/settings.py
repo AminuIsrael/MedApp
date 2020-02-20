6118c4b9bbb67d31cc7bb75526e34ba1018bfdbc
Kelvin <kelvin.adigwu@techadvance.ng>
2020-02-20 16:44:28 +0100

    Added requirements.txt

--- MedApp/settings.py
+++ MedApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6q%409325l!tjf370&(h#1!$&=q=@)n1-5bsgosnd(l$%jp$sl'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
